3ab7596362a648242924674758d854881e6eb05d
EEEP Gerardo José <kaykebarbosadev@gmail.com>
2023-11-02 11:00:55 -0300

    add new commit: .gitignore

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import { createServer } from 'node:http'
 
-const server = createServer(() => {
-  console.log('oi')
+const server = createServer((request, response) => {
+  response.write('hello world')
+
+  return response.end()
 })
 
 server.listen(3333)
