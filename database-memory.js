f04ddd6156713eb1489265c4e2e0142623339785
EEEP Gerardo José <kaykebarbosadev@gmail.com>
2023-11-02 12:07:24 -0300

    add commit: routes and database in memory

---
+++ database-memory.js
@@ -0,0 +1,32 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+  #videos = new Map()
+
+  list(){
+    return Array.from(this.#videos.entries()).map((videoArray) => {
+      const id = videoArray[0]
+      const data = videoArray[1]
+
+      return {
+        id,
+        ...data,
+      }
+    })
+  }
+
+
+  create(video){
+    const videoId = randomUUID()
+
+    this.#videos.set(videoId, video)
+  }
+
+  update(id, video){
+    this.#videos.set(id, video)
+  }
+
+  delete(id){
+    this.#videos.delete(id)
+  }
+}
--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-  return 'Hello World'
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body
+
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+
+  return reply.status(201).send()
 })
 
-server.get('/hello', () => {
-  return 'Hello rocktseat'
+server.get('/videos', () => {
+  const videos = database.list()
+
+  return videos
 })
 
-server.get('/node', () => {
+server.put('/videos/:id', (request, reply) => {
+  const videoId = request.params.id
+  const { title, description, duration } = request.body
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(204).send()
+})
+
+server.delete('/videos/:id', () => {
   return 'Hello node'
 })
 
